f0dd0e07e60067cdc1d7b6aa002e89183ee51228
Blocktrail <blocktrail@digimini.local>
2015-09-23 13:05:19 +0200

    rewording

--- src/js/localisation/translations.js
+++ src/js/localisation/translations.js
@@ -306,9 +306,9 @@ angular.module('blocktrail.localisation', [
             PERMISSION_REQUIRED_CONTACTS: 'Wallet needs access to your contacts',
             PERMISSION_REQUIRED_CAMERA: 'Wallet needs access to your camera',
             PERMISSION_REQUIRED_PHOTOS: 'Wallet needs access to your photos',
-            MSG_CONTACTS_PERMISSIONS: 'To grant access to your contacts:\n on your device go to settings>Blocktrail Wallet>Contacts',
-            MSG_CAMERA_PERMISSIONS: 'To grant access to your camera:\n on your device go to settings>Blocktrail Wallet>App.photo',
-            MSG_PHOTOS_PERMISSIONS: 'To grant access to your photos:\n on your device go to settings>Blocktrail Wallet>Photos'
+            MSG_CONTACTS_PERMISSIONS: 'To grant access to your contacts on your device, go to settings>Blocktrail Wallet>Contacts',
+            MSG_CAMERA_PERMISSIONS: 'To grant access to your camera on your device, go to settings>Blocktrail Wallet>App.photo',
+            MSG_PHOTOS_PERMISSIONS: 'To grant access to your photos on your device, go to settings>Blocktrail Wallet>Photos'
         };
         var americanEnglish = angular.extend(angular.copy(english), {
             MSG_INIT_WALLET: 'initializing wallet...',
