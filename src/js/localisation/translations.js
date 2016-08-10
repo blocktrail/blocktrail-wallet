5be6c9f65b3c05278269f1898108812cfdc16b16
Ruben de Vries <ruben@rubensayshi.com>
2016-08-10 13:57:37 +0200

    concat multiline translations

--- src/js/localisation/translations.js
+++ src/js/localisation/translations.js
@@ -3,6 +3,17 @@ angular.module('blocktrail.localisation', [
     'blocktrail.translations'
 ])
     .config(function($translateProvider, TRANSLATIONS) {
+        var processTranslations = function(translations) {
+            _.forEach(translations, function(v, k) {
+                // merged arrays with newlines
+                if (_.isArray(v)) {
+                    translations[k] = v.join("\n");
+                }
+            });
+
+            return translations;
+        };
+
         //init with device lang as default
         var availableLocales = [
             'en-GB',
@@ -18,10 +29,10 @@ angular.module('blocktrail.localisation', [
         var french = angular.extend({}, english, TRANSLATIONS.french, TRANSLATIONS.mobile.french);
         var dutch = angular.extend({}, english, TRANSLATIONS.dutch, TRANSLATIONS.mobile.dutch);
 
-        $translateProvider.translations('en-GB', english);
-        $translateProvider.translations('en-US', americanEnglish);
-        $translateProvider.translations('fr-FR', french);
-        $translateProvider.translations('nl-NL', dutch);
+        $translateProvider.translations('en-GB', processTranslations(english));
+        $translateProvider.translations('en-US', processTranslations(americanEnglish));
+        $translateProvider.translations('fr-FR', processTranslations(french));
+        $translateProvider.translations('nl-NL', processTranslations(dutch));
 
         $translateProvider.preferredLanguage(defaultLanguage);
     });
