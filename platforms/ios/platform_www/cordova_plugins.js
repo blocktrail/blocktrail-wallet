e0960dd6c0d7d83e2b02abca66d48c6f21d69269
Ruben de Vries <ruben@rubensayshi.com>
2016-12-13 12:57:02 +0100

    swap FB plugin iOS

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -422,14 +422,6 @@ module.exports = [
         ]
     },
     {
-        "file": "plugins/phonegap-facebook-plugin/facebookConnectPlugin.js",
-        "id": "phonegap-facebook-plugin.FacebookConnectPlugin",
-        "pluginId": "phonegap-facebook-plugin",
-        "clobbers": [
-            "facebookConnectPlugin"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-google-app-conversion-tracker/www/gapp_track.js",
         "id": "cordova-plugin-google-app-conversion-tracker.GoogleAppTracking",
         "pluginId": "cordova-plugin-google-app-conversion-tracker",
@@ -477,6 +469,14 @@ module.exports = [
         "clobbers": [
             "window.plugins.tunePlugin"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-facebook4/www/facebook-native.js",
+        "id": "cordova-plugin-facebook4.FacebookConnectPlugin",
+        "pluginId": "cordova-plugin-facebook4",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -509,11 +509,11 @@ module.exports.metadata =
     "cordova-plugin-vibration": "1.2.0",
     "phonegap-plugin-barcodescanner": "4.0.2",
     "cordova-plugin-dialogs": "1.1.2-dev",
-    "phonegap-facebook-plugin": "0.12.0",
     "cordova-plugin-google-app-conversion-tracker": "0.0.2",
     "cordova-fabric-plugin": "1.0.8",
     "cordova-plugin-google-analytics": "1.5.6",
-    "cordova-plugin-tune": "5.0.0"
+    "cordova-plugin-tune": "5.0.0",
+    "cordova-plugin-facebook4": "1.7.4"
 }
 // BOTTOM OF METADATA
 });
