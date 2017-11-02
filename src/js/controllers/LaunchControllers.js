453237ee81e35e7bae95ed3835311bb8740fe440
Ruben de Vries <ruben@rubensayshi.com>
2017-11-02 17:02:15 +0100

    fixed issue with old storage -> new storage

--- src/js/controllers/LaunchControllers.js
+++ src/js/controllers/LaunchControllers.js
@@ -8,6 +8,13 @@ angular.module('blocktrail.wallet')
             disableAnimate: true
         });
 
+        checkStorageVersion()
+            .then(function(continueLoading) {
+                if (continueLoading) {
+                    gotoNextState();
+                }
+            });
+
         /**
          * @returns Promise<bool> TRUE when we can continue loading
          */
@@ -40,80 +47,75 @@ angular.module('blocktrail.wallet')
                 });
         }
 
-        checkStorageVersion()
-            .then(function(continueLoading) {
-                if (!continueLoading) {
-                    return;
-                }
+        function gotoNextState() {
+            return settingsService.$isLoaded()
+                .then(function() {
+                    if (navigator.splashscreen) {
+                        navigator.splashscreen.hide();
+                    }
 
-                return settingsService.$isLoaded();
-            })
-            .then(function() {
-                if (navigator.splashscreen) {
-                    navigator.splashscreen.hide();
-                }
+                    //setup not started yet
+                    if (!settingsService.setupStarted) {
+                        // never show rebrand to user who just got started
+                        settingsService.$isLoaded().then(function () {
+                            settingsService.showRebrandMessage = false;
+                            settingsService.$store();
+                            $state.go('app.setup.start');
+                        });
 
-                //setup not started yet
-                if (!settingsService.setupStarted) {
-                    // never show rebrand to user who just got started
-                    settingsService.$isLoaded().then(function () {
-                        settingsService.showRebrandMessage = false;
-                        settingsService.$store();
-                        $state.go('app.setup.start');
-                    });
+                        return;
+                    }
 
-                    return;
-                }
+                    if (settingsService.showRebrandMessage) {
+                        $state.go('app.rebrand');
+                        return;
+                    }
 
-                if (settingsService.showRebrandMessage) {
-                    $state.go('app.rebrand');
-                    return;
-                }
+                    var nextState = null;
 
-                var nextState = null;
+                    //setup has been started: resume from the relevant step
+                    if (settingsService.setupComplete) {
+                        if ($rootScope.handleOpenURL) {
+                            $log.log("launching app with uri: " + $rootScope.handleOpenURL);
+                            $log.log("bitcoin? " + $rootScope.handleOpenURL.startsWith("bitcoin"));
+                            $log.log("bitcoincash? " + ($rootScope.handleOpenURL.startsWith("bitcoincash") || $rootScope.handleOpenURL.startsWith("bitcoin cash")));
+                            $log.log("glidera? " + $rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback"));
 
-                //setup has been started: resume from the relevant step
-                if (settingsService.setupComplete) {
-                    if ($rootScope.handleOpenURL) {
-                        $log.log("launching app with uri: " + $rootScope.handleOpenURL);
-                        $log.log("bitcoin? " + $rootScope.handleOpenURL.startsWith("bitcoin"));
-                        $log.log("bitcoincash? " + ($rootScope.handleOpenURL.startsWith("bitcoincash") || $rootScope.handleOpenURL.startsWith("bitcoin cash")));
-                        $log.log("glidera? " + $rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback"));
-
-                        if ($rootScope.handleOpenURL.startsWith("bitcoin") || $rootScope.handleOpenURL.startsWith("bitcoincash") || $rootScope.handleOpenURL.startsWith("bitcoin cash")) {
-                            $rootScope.bitcoinuri = $rootScope.handleOpenURL;
-                            nextState = 'app.wallet.send';
-                            $ionicSideMenuDelegate.toggleLeft(false);
-                        } else if ($rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback/oauth2")) {
-                            $rootScope.glideraCallback = $rootScope.handleOpenURL;
-                            nextState = 'app.wallet.buybtc.glidera_oauth2_callback';
-                            $ionicSideMenuDelegate.toggleLeft(false);
-                        } else if ($rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback/return")) {
-                            nextState = 'app.wallet.buybtc.choose';
-                            $ionicSideMenuDelegate.toggleLeft(false);
+                            if ($rootScope.handleOpenURL.startsWith("bitcoin") || $rootScope.handleOpenURL.startsWith("bitcoincash") || $rootScope.handleOpenURL.startsWith("bitcoin cash")) {
+                                $rootScope.bitcoinuri = $rootScope.handleOpenURL;
+                                nextState = 'app.wallet.send';
+                                $ionicSideMenuDelegate.toggleLeft(false);
+                            } else if ($rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback/oauth2")) {
+                                $rootScope.glideraCallback = $rootScope.handleOpenURL;
+                                nextState = 'app.wallet.buybtc.glidera_oauth2_callback';
+                                $ionicSideMenuDelegate.toggleLeft(false);
+                            } else if ($rootScope.handleOpenURL.startsWith("btccomwallet://glideraCallback/return")) {
+                                nextState = 'app.wallet.buybtc.choose';
+                                $ionicSideMenuDelegate.toggleLeft(false);
+                            } else {
+                                nextState = 'app.wallet.summary';
+                            }
                         } else {
                             nextState = 'app.wallet.summary';
                         }
+
+                        $state.go(nextState);
+
+                    } else if (!settingsService.backupSaved && !settingsService.backupSkipped) {
+                        //backup saving
+                        $state.go('app.setup.backup');
+                    } else if (!settingsService.phoneVerified) {
+                        //phone setup
+                        $state.go('app.setup.phone');
+                    } else if (!settingsService.contactsLastSync) {
+                        //contacts sync
+                        $state.go('app.setup.contacts');
                     } else {
-                        nextState = 'app.wallet.summary';
+                        //profile
+                        $state.go('app.setup.profile');
                     }
-
-                    $state.go(nextState);
-
-                } else if (!settingsService.backupSaved && !settingsService.backupSkipped) {
-                    //backup saving
-                    $state.go('app.setup.backup');
-                } else if (!settingsService.phoneVerified) {
-                    //phone setup
-                    $state.go('app.setup.phone');
-                } else if (!settingsService.contactsLastSync) {
-                    //contacts sync
-                    $state.go('app.setup.contacts');
-                } else {
-                    //profile
-                    $state.go('app.setup.profile');
-                }
-            });
+                });
+        }
     });
 
 angular.module('blocktrail.wallet')
