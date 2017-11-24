d4e36a80f7151cb571bfbfcc6639c3ea7b7c4404
Roman <panchenkors@gmail.com>
2017-11-24 13:02:19 +0100

    localstoragefactory (#65)

--- src/js/modules/core/services/local-settings/local-settings.service.js
+++ src/js/modules/core/services/local-settings/local-settings.service.js
@@ -2,12 +2,12 @@
     "use strict";
 
     angular.module("blocktrail.core")
-        .factory("localSettingsService", function($log, CONFIG, helperService, storageService) {
-            return new LocalSettingsService($log, CONFIG, helperService, storageService);
+        .factory("localSettingsService", function($log, CONFIG, helperService, localStorageFactory) {
+            return new LocalSettingsService($log, CONFIG, helperService, localStorageFactory);
         }
     );
 
-    function LocalSettingsService($log, CONFIG, helperService, storageService) {
+    function LocalSettingsService($log, CONFIG, helperService, localStorageFactory) {
         var self = this;
 
         self._$log = $log;
@@ -15,68 +15,33 @@
         self._helperService = helperService;
 
         // Local settings
-        self._keyIdForLocalSettings = "localSettings";
-        self._defaultLocalSettingsDoc = {
-            _id: self._keyIdForLocalSettings,
+        self._localSettingsStorage = localStorageFactory.init("localSettings", {
             isSetupComplete: false,
             isPhoneVerified: false,
             isContactsSynchronized: false,
             // TODO Use it here
             pinOnOpen: false
-        };
-        self._pendingLocalSettings = [];
-        self._promiseLocalSettings = null;
-
-        // Init storage DB
-        self._storage = storageService.db("localSettings");
+        });
     }
 
     /**
-     * Get local settings
+     * Get account info
      * @return { promise<object> }
      */
     LocalSettingsService.prototype.getLocalSettings = function() {
         var self = this;
 
-        self._$log.debug("M:CORE:LocalSettingsService:getLocalSettings");
-
-        return self._storage.get(self._keyIdForLocalSettings)
-            .then(function(doc) { return doc; }, function() { return self._defaultLocalSettingsDoc; });
+        return self._localSettingsStorage.getData();
     };
 
     /**
-     * Set local settings
+     * Set account info
      * @return { promise<true> }
      */
     LocalSettingsService.prototype.setLocalSettings = function(data) {
         var self = this;
 
-        self._$log.debug("M:CORE:LocalSettingsService:setLocalSettings");
-
-        if(self._promiseLocalSettings) {
-            self._helperService.pushPendingData(self._pendingLocalSettings, data);
-            return self._promiseLocalSettings;
-        } else {
-            return self._promiseLocalSettings = self.getLocalSettings()
-                .then(function(doc) {
-                    // Use doc as a schema object
-                    return self._storage.put(self._helperService.prepareObjectAccordingToSchema(doc, data))
-                        .then(function() {
-                            // Unset the promise, it's now safe for another update operation to happen
-                            self._promiseLocalSettings = null;
-
-                            var pendingData = self._helperService.getSquashedPendingData(self._pendingLocalSettings);
-
-                            if (pendingData) {
-                                return self.setLocalSettings(pendingData);
-                            }
-
-                            self._$log.debug("M:CORE:LocalSettingsService:setLocalSettings:success");
-
-                            return true;
-                        });
-                });
-        }
+        return self._localSettingsStorage.setData(data);
     };
 })();
 
