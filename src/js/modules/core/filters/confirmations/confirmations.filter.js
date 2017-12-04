c04f43a3054121251f8c10a86f222db35a0f4917
romaroid <panchenkors@gmail.com>
2017-12-04 12:34:43 +0100

    Fix transaction confirmations

--- src/js/modules/core/filters/confirmations/confirmations.filter.js
+++ src/js/modules/core/filters/confirmations/confirmations.filter.js
@@ -5,11 +5,11 @@
         .filter("confirmations", confirmations);
 
     function confirmations(walletsManagerService) {
-        var activeWallet = walletsManagerService.getActiveWallet();
-        var walletData = activeWallet.getReadOnlyWalletData();
-
         return function(input) {
             if (input) {
+                var activeWallet = walletsManagerService.getActiveWallet();
+                var walletData = activeWallet.getReadOnlyWalletData();
+
                 return (parseInt(walletData.blockHeight) - parseInt(input)) + 1;
             } else {
                 return 0;
