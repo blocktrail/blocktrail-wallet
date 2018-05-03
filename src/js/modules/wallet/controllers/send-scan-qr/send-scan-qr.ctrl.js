904fb5e053820a224d57bd90a17f7c786f61af10
Tobias Hoffmann <lacksfish@gmail.com>
2018-05-03 14:54:19 +0200

    Add BIP70 payment request QR scanning functionality

--- src/js/modules/wallet/controllers/send-scan-qr/send-scan-qr.ctrl.js
+++ src/js/modules/wallet/controllers/send-scan-qr/send-scan-qr.ctrl.js
@@ -13,6 +13,26 @@
 
         $ionicLoading.show({template: "<div>{{ 'LOADING' | translate }}...</div>", hideOnStateChange: true});
 
+        // borrowed from bip21, with a modification for optional addresses
+        // in urls.
+        function decodeBitcoin (uri) {
+            var qregex = /(bitcoin|bitcoincash):\/?\/?([^?]+)?(\?([^]+))?/.exec(uri);
+            if (!qregex) throw new Error('Invalid BIP21 URI: ' + uri);
+
+            var protocol = qregex[1];
+            var address = qregex[2];
+            var query = qregex[4];
+
+            var options = parseQuery("?"+query);
+            if (options.amount) {
+                options.amount = Number(options.amount);
+                if (!isFinite(options.amount)) throw new Error('Invalid amount');
+                if (options.amount < 0) throw new Error('Invalid amount');
+            }
+
+            return { address: address, options: options };
+        }
+
         //wait for transition, then open the scanner and begin scanning
         $timeout(function() {
             QR.scan(
@@ -29,12 +49,18 @@
 
                     $log.debug(elm.protocol, elm.pathname, elm.search, elm.hostname);
 
+                    // Handle cancelled stage
                     if (result.toLowerCase() === "cancelled") {
                         // go back - on iOS
-                        if(ionic.Platform.isIOS()) {
-                            $timeout(function() {$btBackButtonDelegate.goBack();}, 180);
+                        if (ionic.Platform.isIOS()) {
+                            $timeout(function () {
+                                $btBackButtonDelegate.goBack();
+                            }, 180);
                         }
-                    } else if (elm.protocol === 'btccomwallet:') {
+                    }
+
+                    // Handle promocodes
+                    if (elm.protocol === 'btccomwallet:') {
                         var reg = new RegExp(/btccomwallet:\/\/promocode\?code=(.+)/);
                         var res = result.match(reg);
 
@@ -44,16 +70,25 @@
                         throw new Error("Can't send to Bitcoin Cash address with BTC wallet");
                     } else if (elm.protocol === 'bitcoin:' || elm.protocol === 'bitcoincash:') {
                         $scope.clearRecipient();
-                        $scope.sendInput.recipientAddress = elm.pathname;
-                        $scope.sendInput.recipientDisplay = elm.pathname;
-                        $scope.sendInput.recipientSource = 'ScanQR';
-                        //check for bitcoin amount in qsa
-                        if (elm.search) {
-                            var reg = new RegExp(/amount=([0-9]*.[0-9]*)/);
-                            var amount = elm.search.match(reg);
-                            if (amount && amount[1]) {
-                                $scope.sendInput.btcValue = parseFloat(amount[1]);
-                                $scope.setFiat();
+                        // Try to decode payment url
+                        try {
+                            var uri = decodeBitcoin(result);
+                        } catch (e) { }
+
+                        if (uri && uri.options && uri.options.r) {
+                            $scope.sendInput.paymentUrl = uri.options.r;
+                        } else {
+                            $scope.sendInput.recipientAddress = elm.pathname;
+                            $scope.sendInput.recipientDisplay = elm.pathname;
+                            $scope.sendInput.recipientSource = 'ScanQR';
+                            //check for bitcoin amount in qsa
+                            if (elm.search) {
+                                var reg = new RegExp(/amount=([0-9]*.[0-9]*)/);
+                                var amount = elm.search.match(reg);
+                                if (amount && amount[1]) {
+                                    $scope.sendInput.btcValue = parseFloat(amount[1]);
+                                    $scope.setFiat();
+                                }
                             }
                         }
 
