65b9312b1e698bcc78d5553ff833518010257427
lacksfish <lacksfish@gmail.com>
2018-11-13 19:58:52 +0100

    changes to account for new keyboard configurations

--- src/js/app.js
+++ src/js/app.js
@@ -251,8 +251,7 @@ angular.module('blocktrail.wallet').run(
 
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard for form inputs)
         if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            cordova.plugins.Keyboard.disableScroll(true);
+            cordova.plugins.Keyboard.hideFormAccessoryBar(true);
         }
 
         // set the iOS status bar style to light
